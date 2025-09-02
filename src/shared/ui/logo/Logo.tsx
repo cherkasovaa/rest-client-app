7adb426fbf629d6f76388dd5e1792ac07bf779e2
cherkasovaa <alina.ch1202@gmail.com>
2025-09-02 22:47:48 +0300

    feat: implement logo component

---
+++ src/shared/ui/logo/Logo.tsx
@@ -0,0 +1,16 @@
+import { Typography } from '@mui/material';
+
+export const Logo = () => {
+  return (
+    <Typography
+      variant="body1"
+      color="secondary.main"
+      sx={{
+        marginRight: 'auto',
+        fontSize: '1.5rem',
+      }}
+    >
+      RCA
+    </Typography>
+  );
+};
