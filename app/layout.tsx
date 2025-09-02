0b71a98136bc35723bd9f9e034656ffc8cdfd0ea
cherkasovaa <alina.ch1202@gmail.com>
2025-09-02 22:56:37 +0300

    fix: ignore hydration mismatch caused by browser extensions

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body suppressHydrationWarning={true}>
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>
             <CssBaseline />
