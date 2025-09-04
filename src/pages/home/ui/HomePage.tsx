c7d16927563e5ca8868326a7a1bb516c2278ca85
cherkasovaa <alina.ch1202@gmail.com>
2025-09-04 16:04:18 +0300

    feat: render general information on home page

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,5 +1,44 @@
-import { Container } from '@mui/material';
+'use client';
+
+import { TeamSection } from '@/widgets/team-section';
+import { WelcomeCard } from '@/widgets/welcome-card';
+import { Box, Container } from '@mui/material';
+import { useState } from 'react';
 
 export const HomePage = () => {
-  return <Container maxWidth="lg">Screen 1</Container>;
+  const [auth, setAuth] = useState<boolean>(false);
+
+  return (
+    <Box width="100%">
+      <Container
+        maxWidth="lg"
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          flexGrow: '1',
+          height: '100%',
+          p: 2,
+        }}
+      >
+        {/* <button onClick={() => setAuth(!auth)}>Toggle</button> */}
+        {auth ? (
+          <Box>Is Auth</Box>
+        ) : (
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              gap: 6,
+              height: '100%',
+            }}
+          >
+            <WelcomeCard />
+            <TeamSection />
+          </Box>
+        )}
+      </Container>
+    </Box>
+  );
 };
