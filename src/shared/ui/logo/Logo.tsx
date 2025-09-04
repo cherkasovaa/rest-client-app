30b7fa0dcd6a3364efa077edf0cb5bbc7b8bce39
cherkasovaa <alina.ch1202@gmail.com>
2025-09-04 15:56:28 +0300

    feat: implement Logo as link

--- src/shared/ui/logo/Logo.tsx
+++ src/shared/ui/logo/Logo.tsx
@@ -1,13 +1,23 @@
+import { ROUTES } from '@/shared/config/routes';
 import { Typography } from '@mui/material';
+import Link from 'next/link';
 
 export const Logo = () => {
   return (
     <Typography
+      component={Link}
+      href={ROUTES.HOME}
       variant="body1"
-      color="secondary.main"
       sx={{
         marginRight: 'auto',
         fontSize: '1.5rem',
+        color: 'secondary.main',
+        cursor: 'pointer',
+        textDecoration: 'none',
+
+        '&:hover': {
+          color: 'secondary.light',
+        },
       }}
     >
       RCA
