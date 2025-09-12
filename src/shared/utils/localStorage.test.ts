337752fcf12bd47abee69b13ce8fd50b69dea3aa
Akseenja <klasik1995@bk.ru>
2025-09-13 00:17:38 +0300

    test: add tests for functions and methods

---
+++ src/shared/utils/localStorage.test.ts
@@ -0,0 +1,30 @@
+import { afterEach, beforeEach } from 'node:test';
+import { describe, expect, test } from 'vitest';
+import { LS, LS_TEST } from './localStorage';
+
+describe('localStorage', () => {
+  const mock: string = 'testing';
+
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
+  afterEach(() => {
+    localStorage.clear();
+  });
+
+  test('should have data saved on set', () => {
+    LS.set(LS_TEST, mock);
+    const raw = localStorage.getItem(LS_TEST);
+
+    expect(raw).not.toBeNull();
+    expect(raw).toBe(JSON.stringify(mock));
+  });
+
+  test('should extract data on get', () => {
+    localStorage.setItem(LS_TEST, JSON.stringify(mock));
+
+    const result = LS.get(LS_TEST);
+    expect(result).toEqual(mock);
+  });
+});
