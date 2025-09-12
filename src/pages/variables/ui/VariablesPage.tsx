81913c3bd03bc1453caccf9f028c91e795f50db6
Akseenja <klasik1995@bk.ru>
2025-09-13 01:36:26 +0300

    feat: add metadata for variables page

--- src/pages/variables/ui/VariablesPage.tsx
+++ src/pages/variables/ui/VariablesPage.tsx
@@ -1,4 +1,10 @@
 import { VariablesTable } from '@/widgets/variables-table';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'HistoVariables | REST Client App',
+  description: 'Map parameters that are sent by the request',
+};
 
 const VariablesPage = () => {
   return <VariablesTable />;
