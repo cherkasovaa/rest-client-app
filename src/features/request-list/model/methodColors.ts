29b21bc1f509a8272d3f6bfab2ab64e331d3ff1d
cherkasovaa <alina.ch1202@gmail.com>
2025-09-19 21:37:42 +0300

    feat: add patch, head and options methods

--- src/features/request-list/model/methodColors.ts
+++ src/features/request-list/model/methodColors.ts
@@ -3,4 +3,7 @@ export const methodColors: Record<string, string> = {
   POST: 'success.main',
   DELETE: 'error.main',
   PUT: 'warning.main',
+  PATCH: '#f8f430',
+  HEAD: '#c204de',
+  OPTIONS: '#04a0de',
 };
