df12c1168b1507718bfebf8df220ef4055d3eaec
cherkasovaa <alina.ch1202@gmail.com>
2025-09-19 20:56:58 +0300

    fix: dynamic table header rendering

--- src/widgets/request-headers/model/constant.ts
+++ src/widgets/request-headers/model/constant.ts
@@ -1,6 +1,6 @@
 import type { GridColDef } from '@mui/x-data-grid';
 
 export const columns: GridColDef[] = [
-  { field: 'key', headerName: 'variable', flex: 1, editable: true },
+  { field: 'key', headerName: 'requestheaderkey', flex: 1, editable: true },
   { field: 'value', headerName: 'value', flex: 1, editable: true },
 ];
